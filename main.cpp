029a19545ab563d61610552c2f7d583823c5af1c
[Olaszi Bence] <[osziben2@gmail.com]>
2024-09-13 22:45:24 +0200

    Megjelölt hibák javítása, valamint további hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];    // hibás név -> N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'  // '' karakterek helyett "" karakterek kellenek a string elé és mögé
-    for (int i = 0;)    // a for ciklusban mind a 3 ; karaktert ki kell írni, még ha nem is használjuk a többi paramétert
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        b[i] = (i+1) * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;  // az atlag változó adattípusa legyen inkább double a tört átlegértékek miatt + inicializálni is kellene 0-val!
-    for (int i = 0; i < N_ELEMENTS, i++)    // N_ELEMENTS után ; a , helyett!
+    double atlag;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
