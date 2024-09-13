f828a7d8c3688f50810fa332e55a7700c762a830
[Olaszi Bence] <[osziben2@gmail.com]>
2024-09-13 22:28:13 +0200

    Hibák megjelölése a kódban későbbi javításra

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];    // hibás név -> N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'  // '' karakterek helyett "" karakterek kellenek a string elé és mögé
+    for (int i = 0;)    // a for ciklusban mind a 3 ; karaktert ki kell írni, még ha nem is használjuk a többi paramétert
     {
         b[i] = i * 2;
     }
@@ -15,8 +15,8 @@ int main()
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;  // az atlag változó adattípusa legyen inkább double a tört átlegértékek miatt + inicializálni is kellene 0-val!
+    for (int i = 0; i < N_ELEMENTS, i++)    // N_ELEMENTS után ; a , helyett!
     {
         atlag += b[i]
     }
